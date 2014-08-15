e9726c2db7cbcbcb8627bde955617544ac2c432b
nemogiftsun <nirmalgibbs@gmail.com>
2014-08-15 23:48:44 +0200

    pr2 updates

--- script/tutorial_1.py
+++ script/tutorial_1.py
@@ -15,7 +15,7 @@ r (q_init)
 q_goal = [-3.2, -4, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 0.0, 0.5, 0, -0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0, -0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 r (q_goal)
 
-ps.loadObstacleFromUrdf ("iai_maps", "kitchen_area")
+#ps.loadObstacleFromUrdf ("iai_maps", "kitchen_area")
 
 ps.setInitialConfig (q_init)
 ps.addGoalConfig (q_goal)
@@ -26,3 +26,30 @@ pp = PathPlayer (robot.client, r)
 
 pp (0)
 pp (1)
+
+ 'l_gripper_l_parallel_root_joint', 'l_gripper_r_finger_joint', 'l_gripper_r_finger_tip_joint', 'l_gripper_joint', 'l_gripper_r_parallel_root_joint', 'laser_tilt_mount_joint', 'r_shoulder_pan_joint', 'r_shoulder_lift_joint', 'r_upper_arm_roll_joint', 'r_elbow_flex_joint', 'r_forearm_roll_joint', 'r_wrist_flex_joint', 'r_wrist_roll_joint', 'r_gripper_l_finger_joint', 'r_gripper_l_finger_tip_joint', 'r_gripper_l_parallel_root_joint', 'r_gripper_r_finger_joint', 'r_gripper_r_finger_tip_joint', 'r_gripper_joint', 'r_gripper_r_parallel_root_joint']
+
+
+        - l_gripper_motor_slider_joint
+        - l_gripper_motor_screw_joint
+
+        - l_gripper_r_finger_joint
+        - l_gripper_r_finger_tip_joint
+        - l_gripper_joint
+        - laser_tilt_mount_joint
+        - r_shoulder_pan_joint
+        - r_shoulder_lift_joint
+        - r_upper_arm_roll_joint
+        - r_elbow_flex_joint
+        - r_forearm_roll_joint
+        - r_wrist_flex_joint
+        - r_wrist_roll_joint
+        - r_gripper_l_finger_joint
+        - r_gripper_l_finger_tip_joint
+        - r_gripper_motor_slider_joint
+        - r_gripper_motor_screw_joint
+        - r_gripper_r_finger_joint
+        - r_gripper_r_finger_tip_joint
+        - r_gripper_joint
+        - torso_lift_motor_screw_joint
+
